da65f2d6f5ebcf2564130e24a6866a85773b7dae
Albin Rönnkvist <albin.ronnkvist@avarda.com>
2023-04-27 20:56:32 +0200

    Split service config to separate file

--- WebApiClient/Program.cs
+++ WebApiClient/Program.cs
@@ -1,77 +1,15 @@
-using Albin.GrpcCodeFirst.Shared.Contracts;
-using Albin.GrpcCodeFirst.WebApiClient.Options;
+using Albin.GrpcCodeFirst.WebApiClient;
 using Albin.GrpcCodeFirst.WebApiClient.Routes.Version1;
-using Albin.GrpcCodeFirst.WebApiClient.Validators;
-using FluentValidation;
-using Grpc.Core;
-using Grpc.Net.Client.Configuration;
-using ProtoBuf.Grpc.ClientFactory;
-using Albin.GrpcCodeFirst.WebApiClient.Services;
-using Polly;
-using Polly.Contrib.WaitAndRetry;
-using EnterRequest = Albin.GrpcCodeFirst.WebApiClient.Dtos.EnterRequest;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IValidator<EnterRequest>, EnterRequestValidator>();
-builder.Services.AddTransient<ITokenClient, TokenClient>();
-builder.Services.AddSingleton<ITokenProvider, TokenProvider>();
 
-builder.Services.Configure<BouncerOptions>(builder.Configuration.GetSection(nameof(BouncerOptions)));
-builder.Services.Configure<AzureAdOptions>(builder.Configuration.GetSection(nameof(AzureAdOptions)));
-
-var bouncerOptions = builder.Configuration
-    .GetSection(nameof(BouncerOptions))
-    .Get<BouncerOptions>();
-ArgumentNullException.ThrowIfNull(bouncerOptions);
-
-builder.Services.AddCodeFirstGrpcClient<IBouncerService>(opt =>
-{
-    opt.Address = new Uri(bouncerOptions.BaseUrl);
-    opt.ChannelOptionsActions.Add(options =>
-    {
-        options.HttpHandler = new SocketsHttpHandler
-        {
-            PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
-            KeepAlivePingDelay = TimeSpan.FromSeconds(60),
-            KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
-            EnableMultipleHttp2Connections = true
-        };
-        options.ServiceConfig = new ServiceConfig
-        {
-            MethodConfigs = { 
-                new MethodConfig
-                {
-                    Names = { MethodName.Default },
-                    RetryPolicy = new RetryPolicy
-                    {
-                        MaxAttempts = 3,
-                        InitialBackoff = TimeSpan.FromSeconds(1),
-                        MaxBackoff = TimeSpan.FromSeconds(5),
-                        BackoffMultiplier = 1.5,
-                        RetryableStatusCodes = { StatusCode.Unavailable }
-                    }
-                }
-            }
-        };
-    });
-})
-.AddCallCredentials(async (context, metadata, serviceProvider) =>
-{
-    var provider = serviceProvider.GetRequiredService<ITokenProvider>();
-    var token = await provider.GetTokenAsync();
-
-    metadata.Add("Authorization", $"Bearer {token}");
-})
-.EnableCallContextPropagation();
-
-builder.Services.AddHttpClient<ITokenClient, TokenClient>(opt =>
-{
-    opt.BaseAddress = new Uri("https://login.microsoftonline.com/");
-})
-.AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5)))
-.AddTransientHttpErrorPolicy(policy => policy.CircuitBreakerAsync(10, TimeSpan.FromSeconds(10)));
+builder.Services.ConfigureOptions(builder.Configuration);
+builder.Services.ConfigureGrpcClients(builder.Configuration);
+builder.Services.ConfigureHttpClients();
+builder.Services.ConfigureValidators();
+builder.Services.ConfigureInternalServices();
 
 var app = builder.Build();
 
---
+++ WebApiClient/ServiceConfigurationExtensions.cs
@@ -0,0 +1,92 @@
+﻿using Albin.GrpcCodeFirst.Shared.Contracts;
+using Albin.GrpcCodeFirst.WebApiClient.Options;
+using Albin.GrpcCodeFirst.WebApiClient.Services;
+using Albin.GrpcCodeFirst.WebApiClient.Validators;
+using FluentValidation;
+using Grpc.Core;
+using Grpc.Net.Client.Configuration;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+using ProtoBuf.Grpc.ClientFactory;
+using EnterRequest = Albin.GrpcCodeFirst.WebApiClient.Dtos.EnterRequest;
+
+namespace Albin.GrpcCodeFirst.WebApiClient;
+
+public static class ServiceConfigurationExtensions
+{
+    public static void ConfigureOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<BouncerOptions>(configuration.GetSection(nameof(BouncerOptions)));
+        services.Configure<AzureAdOptions>(configuration.GetSection(nameof(AzureAdOptions)));
+    }
+
+    public static void ConfigureValidators(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<EnterRequest>, EnterRequestValidator>();
+    }
+
+    public static void ConfigureGrpcClients(this IServiceCollection services, IConfiguration configuration)
+    {
+        var bouncerOptions = configuration
+            .GetSection(nameof(BouncerOptions))
+            .Get<BouncerOptions>();
+        ArgumentNullException.ThrowIfNull(bouncerOptions);
+
+        services.AddCodeFirstGrpcClient<IBouncerService>(opt =>
+            {
+                opt.Address = new Uri(bouncerOptions.BaseUrl);
+                opt.ChannelOptionsActions.Add(options =>
+                {
+                    options.HttpHandler = new SocketsHttpHandler
+                    {
+                        PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+                        KeepAlivePingDelay = TimeSpan.FromSeconds(60),
+                        KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+                        EnableMultipleHttp2Connections = true
+                    };
+                    options.ServiceConfig = new ServiceConfig
+                    {
+                        MethodConfigs = {
+                            new MethodConfig
+                            {
+                                Names = { MethodName.Default },
+                                RetryPolicy = new RetryPolicy
+                                {
+                                    MaxAttempts = 3,
+                                    InitialBackoff = TimeSpan.FromSeconds(1),
+                                    MaxBackoff = TimeSpan.FromSeconds(5),
+                                    BackoffMultiplier = 1.5,
+                                    RetryableStatusCodes = { StatusCode.Unavailable }
+                                }
+                            }
+                        }
+                    };
+                });
+            })
+            .AddCallCredentials(async (context, metadata, serviceProvider) =>
+            {
+                var provider = serviceProvider.GetRequiredService<ITokenProvider>();
+                var token = await provider.GetTokenAsync();
+
+                metadata.Add("Authorization", $"Bearer {token}");
+            })
+            .EnableCallContextPropagation();
+    }
+
+    public static void ConfigureHttpClients(this IServiceCollection services)
+    {
+        services.AddHttpClient<ITokenClient, TokenClient>(opt =>
+            {
+                opt.BaseAddress = new Uri("https://login.microsoftonline.com/");
+            })
+            .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5)))
+            .AddTransientHttpErrorPolicy(policy => policy.CircuitBreakerAsync(10, TimeSpan.FromSeconds(10)));
+    }
+
+    public static void ConfigureInternalServices(this IServiceCollection services)
+    {
+        services.AddTransient<ITokenClient, TokenClient>();
+        // TODO: use decorator instead of singleton, and use caching + AES encryption to store access token... Scrutor etc...
+        services.AddSingleton<ITokenProvider, TokenProvider>();
+    }
+}
